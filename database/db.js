492a5e95f465ecf5b2c6cd27ebd0597a96ed1d4c
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-06-23 23:06:51 -0400

    Datos predeterminados agregados

--- database/db.js
+++ database/db.js
@@ -1,25 +1,25 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import { set, connect } from 'mongoose';
-// import Users from '../models/Users.js';
-// import Services from '../models/Services';
-// import addAdmin from './dbAdmin'; // Agrega Admin
-// import addServices from './dbServices'; // Agrega los Servicios
+import Users from '../models/Users.js';
+import Services from '../models/Services.js';
+import addAdmin from './dbAdmin.js'; // Agrega Admin
+import addServices from './dbServices.js'; // Agrega los Servicios
 
 set('strictQuery', false)
 
 connect(process.env.URI) 
    .then(async()=> {
-      // const admin = await Users.find({email: process.env.ADMIN_EMAIL}).lean();
-      // const services = await Services.findOne().lean();
-      // if (!admin) {
-      //    addAdmin()
-      //    console.log('Administrador Creado');
-      // }
-      // if (!services) {
-      //    addServices()
-      //    console.log('Servicios Agregados');
-      // }
+      const admin = await Users.findOne({email: process.env.ADMIN_EMAIL}).lean();
+      const services = await Services.findOne().lean();
+      if (!admin) {
+         addAdmin()
+         console.log('Administrador Creado');
+      }
+      if (!services) {
+         addServices()
+         console.log('Servicios Agregados');
+      }
       console.log('DB Conectada🚀')
    })
    .catch((e) => console.log("Fallo de Conexion " + e));
